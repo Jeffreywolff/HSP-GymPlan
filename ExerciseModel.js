b6ab4ac8f264289e19034f0cd5440b3362d793fc
Jeffreywolff <jeffrey.wolff@elev.ga.ntig.se>
2021-03-08 12:38:53 +0100

    Removed part of Form and mongoose schema

--- ExerciseModel.js
+++ ExerciseModel.js
@@ -2,32 +2,17 @@ const mongoose = require('mongoose');
 const ExerciseSchema = new mongoose.Schema({
     area: String,
     exerciseName: String,
-    strenghtSets: Number,
-    strenghtReps: Number,
-    growthSets: Number,
-    growthReps: Number,
-    enduranceSets: Number,
-    enduranceReps: Number,
-    difficulty: Number,
 
   });
 
   const Exercise = mongoose.model('Exercise', ExerciseSchema);
 
 
-exports.newExercise = (area, name, strenghtSets, strenghtReps, 
-  growthSets, growthReps, enduranceSets, enduranceReps, difficulty) =>{
+exports.newExercise = (area, name) =>{
 
     var exercise = new Exercise({
       area: area,
       exerciseName: name,
-      strenghtSets: strenghtSets,
-      strenghtReps: strenghtReps,
-      growthSets: growthSets,
-      growthReps: growthReps,
-      enduranceSets: enduranceSets,
-      enduranceReps: enduranceReps,
-      difficulty: difficulty,
     });
 
     return exercise;
